05b8f6fd19ea6be0eb89a7ec06e6784e26fa1701
lucs7 <s0810498@gmail.com>
2026-09-02 17:23:27 +0200

    refactor(ui): reports/common

--- Pages/Reports/CommonReportsPage.php
+++ Pages/Reports/CommonReportsPage.php
@@ -70,7 +70,7 @@ class CommonReportsPage extends ActionPage implements ICommonReportsPage
     public function ProcessPageLoad()
     {
         $this->Set('DateAxisFormat', Resources::GetInstance()->GetDateFormat('report_date'));
-        $this->Display('Reports/common-reports.tpl');
+        $this->Display('pages/reports/common-reports.twig');
     }
 
     public function BindReport(IReport $report, IReportDefinition $definition, $selectedColumns)
@@ -84,23 +84,23 @@ class CommonReportsPage extends ActionPage implements ICommonReportsPage
     public function ShowCsv()
     {
         $this->Set('ReportCsvColumnView', new ReportCsvColumnView($this->GetVar('SelectedColumns')));
-        $this->DisplayCsv('Reports/custom-csv.tpl', 'report.csv');
+        $this->DisplayCsv('components/reports/custom-csv.twig', 'report.csv');
     }
 
     public function DisplayError()
     {
-        $this->Display('Reports/error.tpl');
+        $this->Display('components/reports/error.twig');
     }
 
     public function ShowResults()
     {
-        $this->Display('Reports/results-custom.tpl');
+        $this->Display('components/reports/results-custom.twig');
     }
 
     public function PrintReport()
     {
         $this->Set('ReportCsvColumnView', new ReportCsvColumnView($this->GetVar('SelectedColumns')));
-        $this->Display('Reports/print-custom-report.tpl');
+        $this->Display('pages/reports/print-custom-report.twig');
     }
 
     /**
